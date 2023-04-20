c606479ffe0a9f812f96d6817459e250ef108046
slesarchukilia <ilia.slesarchuk@dcmail.ca>
2023-04-19 20:20:11 -0400

    git add

---
+++ static_site/js/main.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(
+    function () {
+        $('.slider').slick({
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: true,
+        });
+    }
+);
